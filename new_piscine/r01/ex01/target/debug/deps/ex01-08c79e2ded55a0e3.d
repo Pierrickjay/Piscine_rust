4677b0f9041492fb6ace96fee803fa32e049c34a
Pierrick JAY <pjay@e2r13p9.clusters.42paris.fr>
2023-02-07 16:30:17 +0100

    push from school

--- new_piscine/r01/ex01/target/debug/deps/ex01-08c79e2ded55a0e3.d
+++
@@ -1,7 +0,0 @@
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/new_piscine/r01/ex01/target/debug/deps/ex01-08c79e2ded55a0e3.rmeta: src/lib.rs
-
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/new_piscine/r01/ex01/target/debug/deps/libex01-08c79e2ded55a0e3.rlib: src/lib.rs
-
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/new_piscine/r01/ex01/target/debug/deps/ex01-08c79e2ded55a0e3.d: src/lib.rs
-
-src/lib.rs:
--- new_piscine/r01/ex01/target/debug/deps/ex01-7ee37d1a114e4111.d
+++
@@ -1,5 +0,0 @@
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/new_piscine/r01/ex01/target/debug/deps/ex01-7ee37d1a114e4111: src/lib.rs
-
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/new_piscine/r01/ex01/target/debug/deps/ex01-7ee37d1a114e4111.d: src/lib.rs
-
-src/lib.rs:
