4def95c92f9673173715742743a94f8108192ca3
Pierrick Jay <pierrick.jay1@gmail.com>
2023-02-23 17:38:51 +0100

    push from school

--- r01/ex06/target/debug/deps/ex06-ee65ccacbd33aba2.d
+++
@@ -1,5 +0,0 @@
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/r01/ex06/target/debug/deps/ex06-ee65ccacbd33aba2: src/lib.rs
-
-/mnt/nfs/homes/pjay/Cursus42/Piscine_rust/r01/ex06/target/debug/deps/ex06-ee65ccacbd33aba2.d: src/lib.rs
-
-src/lib.rs:
